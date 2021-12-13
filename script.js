452d632616e34771969c9cfb58a6b9c1b662161d
Froggi22 <daniel.heinstedt@gmail.com>
2021-12-13 01:38:37 +0100

    2021.12.13.0
    Major; Front page centered, media rules reworked, everything validated, commented and cleaned up

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+/* jshint esversion: 6 */
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
+let collapsible = document.getElementsByClassName("collapsible"); // Fetch all collapsible quesion-buttons
+
+for (let i = 0; i < collapsible.length; i++) { // Iterate through all collapsibles
+	collapsible[i].addEventListener("click", function() { // Add click event listeners on each one
+		this.classList.toggle("active");
+		let content = this.nextElementSibling;
+		if (content.style.maxHeight){
+			content.style.maxHeight = null;
+		} else {
+			content.style.maxHeight = content.scrollHeight + "px";
+		} 
+	});
 }
