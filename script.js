cc5d8ea8c62cd8b78405f80db0dc80f00abbe6a8
Froggi22 <daniel.heinstedt@gmail.com>
2022-01-21 15:31:56 +0100

    2022.01.21.0
    Added "dynamic" links, easy to update from script.js file.

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 /* jshint esversion: 6 */
 
-let collapsible = document.getElementsByClassName("collapsible"); // Fetch all collapsible quesion-buttons
+/* ===== Variables / links ===== */
+const discordInviteLink = "https://discord.com/invite/kg7VfRQ9Xw";
+const botInviteLink = "https://discord.com/api/oauth2/authorize?client_id=864572952275714059&permissions=2147600448&scope=bot%20applications.commands";
 
-for (let i = 0; i < collapsible.length; i++) { // Iterate through all collapsibles
-	collapsible[i].addEventListener("click", function() { // Add click event listeners on each one
+const discordInviteLinkElement = document.getElementsByClassName("discordInviteLink"); // Discord Invite Link
+for (let element = 0; element < discordInviteLinkElement.length; element++) {
+	discordInviteLinkElement[element].href = discordInviteLink;
+};
+const botInviteLinkElement = document.getElementsByClassName("botInviteLink"); // Bot Invite Link
+for (let element = 0; element < botInviteLinkElement.length; element++) {
+	botInviteLinkElement[element].href = botInviteLink;
+};
+
+/* ===== FAQ Collapsibles ===== */
+const collapsible = document.getElementsByClassName("collapsible"); // Fetch all collapsible quesion-buttons
+for (let element = 0; element < collapsible.length; element++) { // Iterate through all collapsibles
+	collapsible[element].addEventListener("click", function() { // Add click event listeners on each one
 		this.classList.toggle("active");
 		let content = this.nextElementSibling;
 		if (content.style.maxHeight) {
