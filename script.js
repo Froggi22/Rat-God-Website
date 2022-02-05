2df8461c650eab0e389674b177cf8085c36903cf
Froggi22 <daniel.heinstedt@gmail.com>
2022-02-05 23:32:03 +0100

    Cleanup
    Features maps commands separator > bold.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /* jshint esversion: 6 */ // For validation
 
 /* ===== FAQ Collapsibles ===== */
-const collapsible = document.getElementsByTagName("summary"); // Fetch all collapsible summaries
-for (let elementnr = 0; elementnr < collapsible.length; elementnr++) { // Iterate through all collapsibles
-	collapsible[elementnr].addEventListener("click", function() { // Add click event listeners on each one
-		this.classList.toggle("faq-active"); // Add the class "faq-active" on clicked collapsable
+const summary = document.getElementsByTagName("summary"); // Fetch all summaries
+for (let elementnum = 0; elementnum < summary.length; elementnum++) { // Iterate through all summaries
+	summary[elementnum].addEventListener("click", function() { // Add click event listeners on each one
+		this.classList.toggle("faq-active"); // Add the class "faq-active" on the clicked summary
 	});
 }
