dc613508ceb4a1c4b0ff17c4c6e15447905a5fe3
Froggi22 <daniel.heinstedt@gmail.com>
2022-03-15 10:06:59 +0100

    JS use strict

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+"use strict"
+
 /* ===== FAQ Collapsibles ===== */
-const summary = document.getElementsByTagName("summary"); // Fetch all summaries
+const summary = document.getElementsByTagName("summary") // Fetch all summaries
 for (let elementnum = 0; elementnum < summary.length; elementnum++) { // Iterate through all summaries
 	summary[elementnum].addEventListener("click", function() { // Add click event listeners on each one
-		this.classList.toggle("faq-active"); // Add the class "faq-active" on the clicked summary
+		this.classList.toggle("faq-active") // Add the class "faq-active" on the clicked summary
 	});
 }
