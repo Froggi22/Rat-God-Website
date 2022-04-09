e1e9fee6d22a3a03dff59265cc04ff756a140c40
Froggi22 <daniel.heinstedt@gmail.com>
2022-04-09 14:14:59 +0200

    Background gradients
    General file cleanup/compression.
    Merged 2 FAQ questions into 1.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 /* ===== FAQ Collapsibles ===== */
 const summary = document.getElementsByTagName("summary") // Fetch all summaries
 for (let elementnum = 0; elementnum < summary.length; elementnum++) { // Iterate through all summaries
-	summary[elementnum].addEventListener("click", function() { // Add click event listeners on each one
+	summary[elementnum].addEventListener("click", () => { // Add click event listeners on each one
 		this.classList.toggle("faq-active") // Add the class "faq-active" on the clicked summary
 	});
 }
